43970e1867f123d2a5bbfc9cade77695d0686ebb
George Ciesinski <george.ciesin@gmail.com>
2021-08-12 18:05:49 -0400

    Made adjustments to app.py so that it can be run in debug mode and also in heroku

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'  # Can be PostgreSQL
 # Stop FlaskSQLAlchemy from tracking changes so we can use the base SQLAlchemy tracker instead
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Debug mode
+app.config['DEBUG'] = True
 # This should be secret, not hard coded. Using name for learning purposes
 app.secret_key = 'george'
 # Create an api for flask_restful
@@ -29,7 +31,11 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-	# Initialize SQLAlchemy object
 	db.init_app(app)
-	# Start app
-	app.run(port=5000, debug=True)
+
+	if app.config['DEBUG']:
+		@app.before_first_request
+		def create_tables():
+			db.create_all()
+
+	app.run(port=5000)
